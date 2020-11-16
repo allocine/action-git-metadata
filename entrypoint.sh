7c3897dc1772c8167103396cc0f21db7f2e4e5fd
Rui <ruiteix@yahoo.fr>
2020-11-16 19:34:54 +0100

    Fix deprecated set-env

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ ref_slug() {
 
 [[ $GITHUB_REF =~ "refs/heads/" ]] && _GITHUB_BRANCH=$(echo $GITHUB_REF | cut -d/ -f3) || :
 [[ $GITHUB_REF =~ "refs/tags/" ]] && _GITHUB_TAG=$(echo $GITHUB_REF | cut -d/ -f3) || :
-echo ::set-env name=CI_COMMIT_REF_NAME::${GITHUB_HEAD_REF:-${_GITHUB_BRANCH:-$_GITHUB_TAG}}
-echo ::set-env name=CI_COMMIT_REF_SLUG::$(ref_slug ${GITHUB_HEAD_REF:-${_GITHUB_BRANCH:-$_GITHUB_TAG}})
-echo ::set-env name=CI_COMMIT_BRANCH::${GITHUB_HEAD_REF:-$_GITHUB_BRANCH}
-echo ::set-env name=CI_COMMIT_TAG::${_GITHUB_TAG}
-echo ::set-env name=CI_COMMIT_SHA::${GITHUB_COMMIT_PR_SHA:-$GITHUB_SHA}
+echo "CI_COMMIT_REF_NAME=${GITHUB_HEAD_REF:-${_GITHUB_BRANCH:-$_GITHUB_TAG}}" >> $GITHUB_ENV
+echo "CI_COMMIT_REF_SLUG=$(ref_slug ${GITHUB_HEAD_REF:-${_GITHUB_BRANCH:-$_GITHUB_TAG}})" >> $GITHUB_ENV
+echo "CI_COMMIT_BRANCH=${GITHUB_HEAD_REF:-$_GITHUB_BRANCH}" >> $GITHUB_ENV
+echo "CI_COMMIT_TAG=${_GITHUB_TAG}" >> $GITHUB_ENV
+echo "CI_COMMIT_SHA=${GITHUB_COMMIT_PR_SHA:-$GITHUB_SHA}" >> $GITHUB_ENV
